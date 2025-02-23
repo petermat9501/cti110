aa4ec26233cc6c828c0b0fccc23a3e63d92582a3
petermat9501 <petermat9501@student.faytechcc.edu>
2025-02-22 23:08:05 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_PetermanTravon.py
@@ -0,0 +1,9 @@
+# Travon Peterman
+# 2/22/25
+# P1LAB1
+# Creating a program where the user can enter their first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110!")
+
+                    
